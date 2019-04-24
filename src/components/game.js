7c090e99ef20f69a1b9e877c8068994d60efff22
Vanessa <47366649+vguercio10@users.noreply.github.com>
2019-04-24 19:43:25 -0400

    nav bar completed

--- src/components/game.js
+++ src/components/game.js
@@ -4,8 +4,12 @@ function ClickyGame() {
 return (
     <div className="container">
     <nav className="navbar navbar-light bg-light">
-    <a className="navbar-brand" href="/">Clicky Game</a>
-    </nav>
+        <a className="navbar-brand" href="/">Clicky Game</a>
+        <span className="navbar-center"> Click an Image To Begin</span>
+        <span className="navbar-right">Score: | Top Score: </span>
+        
+  </nav>
+
     
 
     </div>
