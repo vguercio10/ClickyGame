ef4265ef1b45296aec5966bd30d1d3157ac0112d
Vanessa <47366649+vguercio10@users.noreply.github.com>
2019-04-24 17:27:39 -0400

    added components dir in src, in components added game.js, created initial navbar only displaying logo

---
+++ src/components/game.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ClickyGame() {
+return (
+    <div className="container">
+    <nav className="navbar navbar-light bg-light">
+    <a className="navbar-brand" href="/">Clicky Game</a>
+    </nav>
+    
+
+    </div>
+)
+}
+
+export default ClickyGame
