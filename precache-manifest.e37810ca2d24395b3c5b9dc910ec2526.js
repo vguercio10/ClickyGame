7d6365c29f0ed7c2a073d27969879b4f32332e46
Vanessa <47366649+vguercio10@users.noreply.github.com>
2019-04-25 14:58:06 -0400

    Updates

---
+++ precache-manifest.e37810ca2d24395b3c5b9dc910ec2526.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b525cd5fd956b45bbafe746ca8e86ed",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "155a941594c150d4d8ec",
+    "url": "/my-app/static/css/main.751b3ec4.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/my-app/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "155a941594c150d4d8ec",
+    "url": "/my-app/static/js/main.9e2a99e2.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
