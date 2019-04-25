49dab06ea1a90ec77257c1b1f7c726da58c12aa5
Vanessa <47366649+vguercio10@users.noreply.github.com>
2019-04-24 22:23:58 -0400

    all pics added to characters.json

--- src/components/card.js
+++ src/components/card.js
@@ -5,7 +5,7 @@ function Card(props) {
 
     return (
     <div onClick = {props.clickImage} className="col-md-3">
-    <img src={props.imageUrl} style={{width: 150, height: 150}}className="img-responsive" alt="spongebob"/>
+    <img src={props.imageUrl} style={{width: 80, height: 125}}className="img-responsive" alt="spongebob"/>
     </div>
     )
 }
